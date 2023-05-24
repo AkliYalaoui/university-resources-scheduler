0666f2caeef54bc8c61b7bec65947f48757d7cdd
AkliYalaoui <akliyalaoui@gmail.com>
2023-05-24 19:15:34 +0100

    Render deployement settings

--- timeschedulersite/settings.py
+++ timeschedulersite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5$)!yojaeph6q(f_#7@4klc&!r6_-xi==0f7px$kubh%9wcl&z'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-+wjl=(yfq$qn%)i9^*khl6fk!9s4g^=1q03u(osrsk88ce6^i8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
