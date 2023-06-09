bb1d0a2907946bcb6bfe26afd218388ea5232a9b
AkliYalaoui <akliyalaoui@gmail.com>
2023-06-09 20:42:52 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py migrate
 
 echo "from scheduler.models import CustomUser; CustomUser.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
  
-python manage.py collectstatic
+python manage.py collectstatic --no-input
